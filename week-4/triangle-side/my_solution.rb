c36354515745711f0cc29e549c85b41e52a77f97
andriayr <reta.andriayr@gmail.cm>
2016-01-21 00:00:29 -0800

    Refactor triangle side solution

--- week-4/triangle-side/my_solution.rb
+++ week-4/triangle-side/my_solution.rb
@@ -1,3 +1,12 @@
+def valid_triangle?(a,b,c)
+    if a + b > c && a + c > b && b + c > a
+    return true
+    elsif a == 0 && b== 0 && c== 0
+    return false
+end
+end
+valid_triangle?(5,1,0)
+
 # def valid_triangle?(a,b,c)
 #   if a+b=c && + a+b>c & b+c >a
 #     true
@@ -11,15 +20,15 @@
 #   end
 #  valid_triangle?(3,4,5)
 
-def valid_triangle?(a,b,c)
-  if a > 0 && b > 0 && c > 0
-    if a+b>c && a+c>b && b+c >a
-      true
-    else
-      false
-    end
-  else
-    false
-  end
-end
- valid_triangle?(0,4,5)
+# def valid_triangle?(a,b,c)
+#   if a > 0 && b > 0 && c > 0
+#     if a+b>c && a+c>b && b+c >a
+#       true
+#     else
+#       false
+#     end
+#   else
+#     false
+#   end
+# end
+#  valid_triangle?(0,4,5)
