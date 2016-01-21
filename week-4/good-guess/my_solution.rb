51e6762b973669af536341a8340f6e2cebdfab57
andriayr <reta.andriayr@gmail.cm>
2016-01-20 17:51:28 -0800

    initial commit 4.5-1

---
+++ week-4/good-guess/my_solution.rb
@@ -0,0 +1,15 @@
+def good_guess?(integer)
+if integer == 42
+  true
+else
+ false
+end
+  end
+good_guess?(42)
+
+# Good Guess
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
