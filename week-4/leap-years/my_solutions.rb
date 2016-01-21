e7cedf76fcf8e418ed636bfd4544c258fec009cc
andriayr <reta.andriayr@gmail.cm>
2016-01-20 22:48:11 -0800

    initial commit for exercise 4.5-4

---
+++ week-4/leap-years/my_solutions.rb
@@ -0,0 +1,17 @@
+# def leap_year?(year)
+#     if year % 4 == 0
+#         return true
+#     elsif year % 4 !=0
+#         return false
+#     end
+# end
+#     leap_year?(2006)
+
+def leap_year?(year)
+  if year % 4 == 0 && year % 100 != 0 || year % 400 == 0
+    return true
+  else
+    return false
+  end
+end
+    leap_year?(1999)
