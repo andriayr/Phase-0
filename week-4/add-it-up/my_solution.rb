239df983365085e8e973b52ffcf06daef8836ce4
andriayr <reta.andriayr@gmail.cm>
2016-01-22 12:44:45 -0800

    inital commit for 4.7-2

---
+++ week-4/add-it-up/my_solution.rb
@@ -0,0 +1,54 @@
+# Add it up!
+
+# Complete each step below according to the challenge directions and
+# include it in this file. Also make sure everything that isn't code
+# is commented in the file.
+
+# I worked on this challenge [by myself, with: ].
+
+# 0. total Pseudocode
+# make sure all pseudocode is commented out!
+
+# Input:
+# Output:
+# Steps to solve the problem.
+
+
+# 1. total initial solution
+
+def total (array)
+  sum = 0
+  for x in array
+    sum += x
+ end
+ return sum
+end
+total([1, 2, 3, 4, 5, 5, 7])
+
+
+def sentence_maker(sentence)
+    words=''
+   sentence.each{|x| words += x.to_s + ' '}
+words=words.capitalize.strip + '.'
+return words
+end
+sentence_maker(["alaska", "has", "over", 586, "thousand", "miles"])
+
+
+# 3. total refactored solution
+
+
+
+# 4. sentence_maker pseudocode
+# make sure all pseudocode is commented out!
+# Input:
+# Output:
+# Steps to solve the problem.
+
+
+# 5. sentence_maker initial solution
+
+
+
+# 6. sentence_maker refactored solution
+
