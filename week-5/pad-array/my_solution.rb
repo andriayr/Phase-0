4d2488ee07dc1dc42a922879f66e44800f5ba63d
andriayr <reta.andriayr@gmail.cm>
2016-01-28 11:50:07 -0800

    initial commit for 5.2

---
+++ week-5/pad-array/my_solution.rb
@@ -0,0 +1,97 @@
+
+# Pad an Array
+
+# I worked on this challenge [ with: Eddie Bueno]
+
+# I spent [1.5] hours on this challenge.
+
+
+# Complete each step below according to the challenge directions and
+# include it in this file. Also make sure everything that isn't code
+# is commented in the file.
+
+
+
+# 0. Pseudocode
+
+# What is the input?
+# Array, Integer, and Optional Value
+
+# What is the output? (i.e. What should the code return?)
+# An array. The same one for destructive and a new array for non-destructive
+
+# What are the steps needed to solve the problem?
+# def method (array, min_size, value)
+#     if min_size <= array.length
+#        then return array
+#     else
+#        array padded to min size
+
+
+# 1. Initial Solution
+
+def pad!(array, min_size, value = nil) #destructive
+    counter = array.length
+    until counter == min_size
+        array.push(value)
+        counter +=1
+    end
+    return array
+end
+pad!([1,2,3,], 5, "apple")
+print pad!
+
+
+# def pad(array, min_size, value = nil) #non-destructive
+#     new_array=Array.new(array)
+#     counter = new_array.length
+#     until counter == min_size
+#         new_array.push(value)
+#         counter +=1
+#     end
+#     return new_array
+# end
+# pad!([1,2,3,], 5, "apple")
+
+def pad!(array, min_size, value = nil) #destructive
+ # Your code here
+ if array.length < min_size
+     for i in 1 .. (min_size - array.length)
+         array.push(value)
+         i+=1
+     end
+     return array
+ else
+     return array
+ end
+end
+
+def pad(array, min_size, value = nil) #non-destructive
+ # Your code here
+ new_array = Array.new(array)
+ if array.length < min_size
+     for i in 1 .. (min_size - array.length)
+         new_array.push(value)
+         i+=1
+     end
+     return new_array
+ else
+     return new_array
+ end
+end
+
+
+3. Refactored Solution
+new_array=Array.new(array)
+return pad! (new_array,min_size,value)
+end
+
+
+# 4. Reflection
+# Were you successful in breaking the problem down into small steps? Yes
+# Once you had written your pseudocode, were you able to easily translate it into code? What difficulties and successes did you have? Yes, the difficulty was with the non-destructive pad, we kept getting a message that says it was being treated as a destructive pad. At that time we identified the array.new(array) which solved that problem.
+# Was your initial solution successful at passing the tests? If so, why do you think that is? If not, what were the errors you encountered and what did you do to resolve them? No, response included above.
+# When you refactored, did you find any existing methods in Ruby to clean up your code? No, all we had to do was cut down the method we created.
+# How readable is your solution? Did you and your pair choose descriptive variable names? I think our solution is pretty readable. Our variables were clear in understanding what we were doing.
+# What is the difference between destructive and non-destructive methods in your own words? Non-destructive does not permanemently change the data structure, while destructive does.
+
